dfb75912a56bdc22c3f251b1c5ddbaee8aed4764
Weizhen <weizhen199@vip.qq.com>
2016-12-22 14:01:04 +0800

    添加项目文件。

---
+++ ExGentleman/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ExGentleman
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Accented;
+        }
+    }
+}
